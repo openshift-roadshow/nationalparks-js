fdbcd28c198db8ee1f80dbf7be8739964cf1227a
jankleinert <jan.kleinert@redhat.com>
2019-01-03 09:57:15 -0500

    Update for compatibility with service binding for mongodb.

--- bin/config.js
+++ bin/config.js
@@ -1,5 +1,20 @@
 var multipaas   = require('config-multipaas');
+var host_from_uri, port_from_uri;
 var autoconfig  = function (config_overrides){
+  // If uri is set, use it to parse out the host and port
+  // otherwise use DB_HOST and DB_PORT
+  // uri comes in the form (mongodb://127.0.0.1:27017)
+  
+  if (process.env.uri) {
+    var pattern = /mongodb?:\/\/([^:^/]*):?(\d*)?/;
+    var match = process.env.uri.match(pattern);
+    
+    if (match !== null && match.length > 0){
+      host_from_uri = match[1];
+      port_from_uri = match[2];	
+    }
+  }
+
   var config    = multipaas(config_overrides).add({
     uri: process.env.uri || process.env.DB_USERNAME || 'mongodb',
     username: process.env.MONGODB_USER || process.env.DB_USERNAME || 'mongodb',
@@ -7,8 +22,8 @@ var autoconfig  = function (config_overrides){
     table_name: process.env.MONGODB_DATABASE || process.env.DB_NAME || 'mongodb',
     collection_name: process.env.MONGODB_DATABASE || process.env.DB_NAME || 'mongodb',
     db_autoload: process.env.DB_AUTOLOAD || "false",
-    db_host: process.env.DB_HOST || "mongodb",
-    db_port: process.env.DB_PORT || "27017",
+    db_host: host_from_uri || process.env.DB_HOST || "mongodb",
+    db_port: port_from_uri || process.env.DB_PORT || "27017",
     db_svc_name: process.env.DATABASE_SERVICE_NAME || process.env.DB_HOST || "mongodb"
   })
 
